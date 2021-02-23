da4572e4d2d740c8df4d185c784a804982175d2b
HeyWeiPan <panwei303031816@gmail.com>
2021-02-23 22:32:17 +0800

    Create FlashLoanReceiverBase.sol

---
+++ FlashLoanReceiverBase.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity >=0.6.6;
+
+import {IFlashLoanReceiver, ILendingPoolAddressesProvider, ILendingPool, IERC20  } from "./MyInterfaces.sol";
+import { SafeERC20, SafeMath } from "./MyLibraries.sol";
+
+abstract contract FlashLoanReceiverBase is IFlashLoanReceiver {
+  using SafeERC20 for IERC20;
+  using SafeMath for uint256;
+
+  ILendingPoolAddressesProvider internal _addressesProvider;
+  ILendingPool internal LENDING_POOL;
+
+  constructor(ILendingPoolAddressesProvider provider) internal {
+    _addressesProvider = provider;
+    LENDING_POOL = ILendingPool(ILendingPoolAddressesProvider(provider).getLendingPool());
+  }
+}
